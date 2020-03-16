478bb1a5aa20c75a669882e8fc6e7bd7b6ec412b
Carl Corsini <carlcorsin@yahoo.com>
2020-03-15 19:22:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+  let clicker = document.querySelector('#clicker');
+  let square = document.querySelector('#square');
+
+  clicker.addEventListener('click', () => {
+    console.log('i think i got it');
+    square.style.backgroundColor = 'aqua';
+  });
+};
+
+// Master
+
+// Algodon id: 3
+
+// - History id: 3, civ_id: 3
+//   - Origins
+//   - Pre Cat
+//   - Recent Past
+//   - Current Events
+
+// history: [
+//   {
+//     id: 1,
+//     title: 'Massacre of Shamony',
+//     date: 'sep 23, 1934',
+//     involved_parties: [1, 2, 6, 7],
+//     body:
+//       'asdfosadfioasjdflkasjdflk asdlkfjaslkdfj askdjf aslkdfj askldf alskdf alskdjasdf',
+//     location: '',
+//   },
+// ];
+
+// - Economy #3 #2
+//   - Clean Money
+//   - Dirty Money
+
+// - NPCs #3 #3
+//   - Commander Faber id: 1, civ_id: 3, neib_id: 1,
+// - Neighborhoods #3 #4
+//   - Sleepy Hollow #1
+// - Quests #3 #5
+
